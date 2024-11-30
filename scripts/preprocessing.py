2f54a2c17baca7855539fa9e43f0f1d7c8800341
Gianmaria Rovelli <rovelli.gianmaria@gmail.com>
2024-11-30 20:06:26 +0100

    init2

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -7,7 +7,7 @@ import typing
 # parsed_address_street_number,parsed_address_unit,parsed_address_postal_code,parsed_address_city,
 # parsed_address_state,parsed_address_country,
 # 3: party_iban,party_phone,external_id
-class ExternalEntity:
+class Party:
     tid: str = ""
     role: str = ""
     iban: str = ""
@@ -36,10 +36,22 @@ class ExternalEntity:
         txt += f"ROLE: {self.role}\n"
         txt += f"IBAN: {self.iban}\n"
         txt += f"INFO: {self.info_unstructured}\n"
-        txt += f"PHONE: {self.phone}\n"
+        txt += f"PHONE: {self.phone} -> {self.format_phone()}\n"
         txt += f"EID: {self.eid}\n"
         txt += "=" * 20
         return txt
+
+    def format_phone_aux(self, num: str):
+        res = "".join([ele for ele in num if ele.isdigit()])
+        return res.strip("0")
+
+    def format_phone(self):
+        ss = self.phone.split(")")
+        res = ""
+        for s in ss:
+            res += self.format_phone_aux(s)
+        return res
+
 
     def to_dict(self):
         return {
@@ -57,30 +69,211 @@ class ExternalEntity:
             "addr_state": self.paddress_state,
             "addr_country": self.paddress_country,
 
-            "phone": self.phone,
+            "phone": self.format_phone(),
             "eid": self.eid
         }
 
     def to_pd(self):
         return pd.DataFrame(self.to_dict(), index=[0])
 
-def herot():
+class Entity:
+    def __init__(self):
+        self.parties: typing.List[Party] = []
 
-    pass
+    def add_party(self, party: Party):
+        self.parties.append(party)
+
+    def __str__(self):  
+        txt = ""
+        for p in self.parties:
+            txt += p.__str__() + "\n"
+            txt += "=" * 20 + "\n"
+        print(len(self.parties))
+        txt += "-" * 30 + "\n"
+        return txt
+
+"""
+group by same info field
+assumption: a party is an entity
+"""
+def byinfo(data: typing.List[Entity]) -> typing.List[Entity]:
+
+    group: typing.Dict[str, typing.List[Entity]] = {}
+
+    print(len(data))
+
+    # group by phone 
+    for bigent in data:
+        for p in bigent.parties:
+            criteria = p.info_unstructured
+            if criteria in group:
+                # put the whole entity
+                if bigent not in group[criteria]:
+                    group[criteria].append(bigent)
+            else:
+                group[criteria] = [bigent]
+
+
+    print(len(group))
+    count = 0
+    tot = 0 
+    entities: typing.List[Entity] = []
+    # merge by group
+    for i in group:
+        bigent: Entity = Entity()
+        for ent in group[i]:
+            bigent.parties += ent.parties
+
+        entities.append(bigent)
+
+        if len(group[i]) > 1:
+            count +=1 
+            tot += len(group[i])
+
+    print(f"by info\n\t{tot} -> {count}\n\t{len(data)} -> {len(entities)}")
+
+    assert len(data) >= len(entities)
+    return entities
+
+"""
+group by same phone field
+assumption: a party is an entity
+"""
+def byphone(data: typing.List[Entity]) -> typing.List[Entity]:
+
+    group: typing.Dict[str, typing.List[Entity]] = {}
+
+    print(len(data))
+
+    # group by phone 
+    for bigent in data:
+        for p in bigent.parties:
+            phone = p.format_phone()
+            if phone in group:
+                # put the whole entity
+                if bigent not in group[phone]:
+                    group[phone].append(bigent)
+            else:
+                group[phone] = [bigent]
+
+
+    print(len(group))
+    count = 0
+    tot = 0 
+    entities: typing.List[Entity] = []
+    # merge by group
+    for i in group:
+        bigent: Entity = Entity()
+        for ent in group[i]:
+            bigent.parties += ent.parties
+
+        entities.append(bigent)
+
+        if len(group[i]) > 1:
+            count +=1 
+            tot += len(group[i])
+
+    print(f"by phone\n\t{tot} -> {count}\n\t{len(data)} -> {len(entities)}")
+
+    assert len(data) >= len(entities)
+    return entities
+
+"""
+group by same iban field
+assumption: a party is an entity
+"""
+def byiban(data: typing.List[Entity]) -> typing.List[Entity]:
+
+    group: typing.Dict[str, typing.List[Entity]] = {}
+
+    # group by info 
+    for bigent in data:
+        for p in bigent.parties:
+            criteria = p.iban
+            if criteria in group:
+                # put the whole entity
+                if bigent not in group[criteria]:
+                    group[criteria].append(bigent)
+            else:
+                group[criteria] = [bigent]
+
+
+    print("iban groups", len(group))
+    count = 0
+    tot = 0 
+    entities: typing.List[Entity] = []
+    revmap: typing.Dict[Entity, Entity] = {}
+
+    # merge by group
+    for i in group:
+        bigent: Entity = Entity()
+        collect: None | Entity = None
+        should_add = False
+        # collect 
+        for ent in group[i]:
+            if ent in revmap:
+                collect = revmap[ent]
+            bigent.parties += ent.parties
+
+        if collect == None:
+            collect = bigent
+            should_add = True
+        for ent in group[i]:
+            if ent not in revmap:
+                collect.parties += ent.parties
+                revmap[ent] = collect
+
+            # for party in ent.parties:
+            #     bigent.add_party(party)
+
+        if should_add:
+            entities.append(collect)
+
+        if len(group[i]) > 1:
+            count +=1 
+            tot += len(group[i])
+
+    print(f"by iban\n\t{tot} -> {count}\n\t{len(data)} -> {len(entities)}")
+
+    assert len(data) >= len(entities)
+    return entities
+
+
+def group_by_eid(eid_to_party):
+    ss = 0
+    count = 0
+    for ep in eid_to_party:
+        ss += len(eid_to_party[ep]) 
+        count += 1
+        if len(eid_to_party[ep]) > 1:
+            print(ep)
+            for p in eid_to_party[ep]:
+                for pp in p.parties:
+                    print(pp.__str__())
+            print(len(eid_to_party[ep]))
+            print("-"*30 + "\n")
+
+    print(ss/count)
 
 
 
-def parse_external_entity(line) -> ExternalEntity:
+def parse_external_entity(line) -> Entity:
 
-    entity: ExternalEntity = ExternalEntity()
-    entity.tid = line[0]
-    entity.role = line[1]
-    entity.info_unstructured= line[2]
-    entity.iban = line[11]
-    entity.phone= line[12]
-    entity.eid= line[13]
+    party: Party = Party()
+    party.tid = line[0]
+    party.role = line[1]
+    party.info_unstructured= line[2]
+    party.iban = line[11]
+    party.phone= line[12]
+    party.eid= line[13]
 
-    return entity
+    entity: Entity = Entity()
+    entity.add_party(party)
+    return entity 
+
+def print_list(entities: typing.List[Entity]):
+    for l in entities:
+        print(l)
             
 
 def main():
@@ -93,7 +286,7 @@ def main():
     eids = {}
     aids = {}
 
-    external:typing.Dict[str, ExternalEntity]  = {}
+    entities:typing.Dict[str, Entity]  = {}
 
     eid_to_party = {}
 
@@ -108,8 +301,7 @@ def main():
             continue
         id = line[0]
         eids[id] = True
-        external[id] = parse_external_entity(line)
-        # print(external[id].to_pd())
+        entities[id] = parse_external_entity(line)
 
 
     # two legs
@@ -121,8 +313,8 @@ def main():
         id = line[0]
         if id in aids:
             twolegs.append(id)
-            if id in external:
-                del external[id]
+            if id in entities:
+                del entities[id]
 
         aids[id] = True
         if id not in eids:
@@ -134,33 +326,37 @@ def main():
         if id not in aids:
             sus.append(id)
 
-    for id in external:
-        elem = external[id]
-        if elem.eid not in eid_to_party:
-            eid_to_party[elem.eid] = [elem]
+    entities_list = []
+    for id in entities:
+        elem = entities[id]
+        entities_list.append(elem)
+        if elem.parties[0].eid not in eid_to_party:
+            eid_to_party[elem.parties[0].eid] = [elem]
         else:
-            eid_to_party[elem.eid].append(elem)
+            eid_to_party[elem.parties[0].eid].append(elem)
 
             # print(line)
-    print(len(twolegs))
-    print(len(orphans))
+    # print(len(twolegs))
+    # print(len(orphans))
     assert len(twolegs) == len(orphans) / 2
 
     ept.close()
     abt.close()
 
-    ss = 0
-    count = 0
-    for ep in eid_to_party:
-        ss += len(eid_to_party[ep]) 
-        count += 1
-        if len(eid_to_party[ep]) > 1:
-            print(ep)
-            for p in eid_to_party[ep]:
-                print(p.__str__())
-            print(len(eid_to_party[ep]))
-            print("-"*30 + "\n")
+    # group_by_eid(eid_to_party)
 
-    print(ss/count)
+    print(len(entities_list))
+    # print(len(entities_list[0].parties))
+    entities_list = byinfo(entities_list);
+    print(len(entities_list))
+    print("="* 40)
+    entities_list = byphone(entities_list);
+    print(len(entities_list))
+    print("="* 40)
+    entities_list = byiban(entities_list);
+    print(len(entities_list))
+    print("="* 40)
+
+    print_list(entities_list);
 
 main()
